a712c84b869c519358d756dce953f9c6ddbad35c
spero61 <spero61@gmail.com>
2021-07-30 18:13:31 +0900

    added toggle_date_button
    comment correction

--- chapter16/exercise16_06.cpp
+++ chapter16/exercise16_06.cpp
@@ -4,6 +4,10 @@
 // that give you the time of day and a way of waiting for a short period of time
 // and to learn to use them based on the documentation you found.
 
+// REVIEW: probably Clock_window class is too big.
+// However to divide this class into pieces, techniques regarding to pointers are required.
+// Those are going to be covered in Chapters 17-18.
+
 #define _CRT_SECURE_NO_WARNINGS  // to disable deprecation warning of the Visual Studio 2019
 
 #include "GUI.h"
@@ -41,19 +45,26 @@ class Clock_window : public Window  // Lines_window inherits from Window
     int minutes;
     int hours;
     string current_time;
+    string current_date;
+    bool is_date_label_on;
+    Vector_ref<Graph_lib::Text> date_labels;
     // widget:
-    Button quit_button;  // end program
+    Button toggle_date_button;  // show and hide date_labels[i]
+    Button quit_button;         // end program
 
     void draw_clock_base();  // draw clock base(frame, second_lines, hour_lines, and hour_labels)
     void start();            // start draw clock hands and refresh(using Fl::wait(), redraw() and sleep_for()) every seconds
+    void toggle_date();
     void quit() { hide(); }  // https://www.fltk.org/doc-1.3/classFl__Window.html#acd69a335bdc69f5624301f267ca7f94e
 };
 
 Clock_window::Clock_window(Point xy, int w, int h, const string& title, Point center, int radius)
-    : Window{xy, w, h, title}, c{center}, r{radius}, frame{c, r}, pin{c, 8},
+    : Window{xy, w, h, title}, c{center}, r{radius}, frame{c, r}, pin{c, 8}, is_date_label_on{false},
       // construct/initialize the parts of the window:
       // location, size, name, action (using lambda expressions)
+      toggle_date_button{Point{x_max() - 160, 0}, 80, 20, "Toggle Date", [](Address, Address pw) { reference_to<Clock_window>(pw).toggle_date(); }},
       quit_button{Point{x_max() - 70, 0}, 70, 20, "Quit", [](Address, Address pw) { reference_to<Clock_window>(pw).quit(); }} {
+    attach(toggle_date_button);
     attach(quit_button);
 
     // get local time from the system
@@ -69,6 +80,7 @@ Clock_window::Clock_window(Point xy, int w, int h, const string& title, Point ce
     minutes = stoi(current_time.substr(14, 2));  // std::string::substr https://www.cplusplus.com/reference/string/string/substr/
     seconds = stoi(current_time.substr(17, 2));
 
+    current_date = current_time.substr(0, 10) + ' ' + current_time.substr(20, 4);
     draw_clock_base();
     start();
 }
@@ -153,24 +165,24 @@ void Clock_window::start() {
         const int x1{static_cast<int>(c.x + d1 * sin(6 * seconds * PI / 180.0))};  // degrees: 30 * i
         const int y1{static_cast<int>(c.y - d1 * cos(6 * seconds * PI / 180.0))};
         second_hands.push_back(new Graph_lib::Line{c, Point{x1, y1}});
-        second_hands[second_hands.size() - 1].set_color(Color::magenta);
-        second_hands[second_hands.size() - 1].set_style(Line_style{Line_style::solid, 1});
+        second_hands[second_hands.size() - 1].set_color(Color{246});
+        second_hands[second_hands.size() - 1].set_style(Line_style{Line_style::solid, 2});
         attach(second_hands[second_hands.size() - 1]);
 
         const int d2{static_cast<int>(r * 0.7)};                                                      // distance from the center of a circle
         const int x2{static_cast<int>(c.x + d2 * sin(6 * (minutes + seconds / 60.0) * PI / 180.0))};  // degrees: 30 * i
         const int y2{static_cast<int>(c.y - d2 * cos(6 * (minutes + seconds / 60.0) * PI / 180.0))};
         minute_hands.push_back(new Graph_lib::Line{c, Point{x2, y2}});
-        minute_hands[minute_hands.size() - 1].set_color(Color::cyan);
-        minute_hands[minute_hands.size() - 1].set_style(Line_style{Line_style::solid, 3});
+        minute_hands[minute_hands.size() - 1].set_color(Color{212});
+        minute_hands[minute_hands.size() - 1].set_style(Line_style{Line_style::solid, 4});
         attach(minute_hands[minute_hands.size() - 1]);
 
         const int d3{static_cast<int>(r * 0.45)};                                                      // distance from the center of a circle
         const int x3{static_cast<int>(c.x + d3 * sin((30 * (hours + minutes / 60.0)) * PI / 180.0))};  // degrees: 30 * i
         const int y3{static_cast<int>(c.y - d3 * cos((30 * (hours + minutes / 60.0)) * PI / 180.0))};
         hour_hands.push_back(new Graph_lib::Line{c, Point{x3, y3}});
-        hour_hands[hour_hands.size() - 1].set_color(Color::yellow);
-        hour_hands[hour_hands.size() - 1].set_style(Line_style{Line_style::solid, 5});
+        hour_hands[hour_hands.size() - 1].set_color(Color{215});
+        hour_hands[hour_hands.size() - 1].set_style(Line_style{Line_style::solid, 6});
         attach(hour_hands[hour_hands.size() - 1]);
 
         pin.set_color(Color{8});  // https://www.fltk.org/doc-1.3/fltk-colormap.png
@@ -199,6 +211,20 @@ void Clock_window::start() {
     }
 }
 
+void Clock_window::toggle_date() {
+    if (!is_date_label_on) {
+        is_date_label_on = true;
+        date_labels.push_back(new Text{Point{c.x - 85, c.y + 185}, current_date});
+        date_labels[date_labels.size() - 1].set_color(Color{207});
+        date_labels[date_labels.size() - 1].set_font(Font::helvetica_bold_italic);
+        date_labels[date_labels.size() - 1].set_font_size(28);
+        attach(date_labels[date_labels.size() - 1]);
+    } else {
+        is_date_label_on = false;
+        detach(date_labels[date_labels.size() - 1]);
+    }
+}
+
 int main() {
     try {
         constexpr int width{800};
@@ -207,7 +233,6 @@ int main() {
 
         Clock_window win{Point{100, 100}, width, height, "Analog Clock", Point{width / 2, height / 2}, radius};
         return gui_main();
-
     } catch (exception& e) {
         cerr << "exception: " << e.what() << '\n';
         return 1;
