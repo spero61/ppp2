34b918761d03a7cc71eed00bcd9bde5b1e05893d
spero61 <spero61@gmail.com>
2021-08-16 00:41:49 +0900

    change stdio.h to iostream

--- chapter17/exercise17_04.cpp
+++ chapter17/exercise17_04.cpp
@@ -1,8 +1,19 @@
-// copy C-style string
+// copy C-style string without using standard library functions
 
 #include <iostream>
 
-char* strdup(const char* s, int n) {
+// instead of strlen()
+int string_length(const char* s) {
+    int count = 0;
+    while (*s != '\0') {
+        count++;
+        *s++;
+    }
+    return count;
+}
+
+char* strdup(const char* s) {
+    int n = string_length(s);
     char* dup = new char[n];
     for (int i = 0; i < n; ++i) {
         dup[i] = s[i];
@@ -11,12 +22,20 @@ char* strdup(const char* s, int n) {
 }
 
 int main() {
-    char today[] = "Today is August 15, 2021 :)";  // C++ version of C-style string
-    printf("%s\n", today);
+    try {
+        char today[] = "Today is August 15, 2021 :)";  // C++ version of C-style string
+        printf("%s\n", today);
 
-    char* dup = strdup(today, sizeof(today));
-    printf("%s\n", dup);
-    delete[] dup;
+        char* copied_today = strdup(today);
+        printf("%s\n", copied_today);
+        delete[] copied_today;
 
-    return 0;
+        return 0;
+    } catch (std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    } catch (...) {
+        std::cerr << "...Exception Occurred\n";
+        return 2;
+    }
 }
