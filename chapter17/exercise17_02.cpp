46f0e9faa5beb394925d84ef1cd6eca63c121813
spero61 <spero61@gmail.com>
2021-08-17 19:57:37 +0900

    usage of reinterpret_cast

--- chapter17/exercise17_02.cpp
+++ chapter17/exercise17_02.cpp
@@ -1,7 +1,44 @@
+// calculate the size (in byte) of types without using sizeof() except to verify the answer
 #include <iostream>
+
+int size(int) {  // just use the type of the argument, not its value
+    int* p = new int[2];
+    int sz = reinterpret_cast<char*>(&p[1]) - reinterpret_cast<char*>(&p[0]);  // turn pointers into char*s (that is byte addresses)
+    // Note that plain &p[1] - &p[0] is 0. Subtracting pointers to elements of an array gives the number of elements between the two pointers.
+    delete[] p;
+    return sz;  // the size
+}
+
+int size(double) {
+    double* p = new double[2];
+    int sz = reinterpret_cast<bool*>(&p[1]) - reinterpret_cast<bool*>(&p[0]);  // also works, because bool is also 1 byte
+    delete[] p;
+    return sz;
+}
+
+int size(bool) {
+    bool* p = new bool[2];
+    int sz = reinterpret_cast<char*>(&p[1]) - reinterpret_cast<char*>(&p[0]);
+    delete[] p;
+    return sz;
+}
+
+int size(char) {
+    char* p = new char[2];
+    int sz = reinterpret_cast<char*>(&p[1]) - reinterpret_cast<char*>(&p[0]);
+    delete[] p;
+    return sz;
+}
 
 int main() {
     try {
+        std::cout << "size(1): " << size(1) << '\n';
+        std::cout << "size(0.1): " << size(0.1) << '\n';
+        std::cout << "size(true): " << size(true) << '\n';
+        std::cout << "size('$'): " << size('$') << "\n\n";
+
+        // verify the answer
+        std::cout << "Verify the answer using sizeof():\n";
         std::cout << "sizeof(char): " << sizeof(char) << '\n'
                   << "sizeof(bool): " << sizeof(bool) << "\n\n"
 
