8ec8eee2fc22ede8dee08a8775cd0f7bbc6a2897
spero61 <spero61@gmail.com>
2021-10-19 21:41:39 +0900

    implement findx() without using STL, subscripting

--- chapter18/exercise18_02.cpp
+++ chapter18/exercise18_02.cpp
@@ -19,21 +19,28 @@ char* findx(const char* s, const char* x) {  // find string x in string s, retur
     char* tmp_x = const_cast<char*>(x);  // it is necessary to remove const from x and y respectively
     for (int i = 0; i < s_len - x_len; ++i) {
         for (int j = 0; j < x_len; ++j) {
-            if (s[i + j] != x[j]) {  // To Do
-                break;
+            if (*tmp_s == *tmp_x) {
+                ++count;
             }
-            ++count;
+            ++tmp_s;
+            ++tmp_x;
         }
+        tmp_s -= x_len;
+        tmp_x -= x_len;
+
         if (count == x_len)
-            return tmp_s += i;  // Note: might be better to change the return type to 'const char*' instead of using const_cast
+            return tmp_s;  // Note: might be better to change the return type to 'const char*' instead of using const_cast
         count = 0;
+        ++tmp_s;
     }
-    return nullptr;  // return nullptr for "not found"
+
+    tmp_s -= (s_len - x_len);  // set pointer to the first element of the array s;
+    return nullptr;            // return nullptr for "not found"
 }
 
 int main() {
     try {
-        char greetings[] = "7:22 PM Tuesday, August 17, 2021 Sunset";  // C++ version of C-style string
+        char greetings[] = "5:50 PM Tuesday, October 19, 2021 Sunset";  // C++ version of C-style string
         char x[] = "day";
         printf("%s\n", greetings);
 
