08bcbe8246c688d12e0ec602b8653c00abcb844b
spero61 <spero61@gmail.com>
2021-10-21 17:17:00 +0900

    Fixed to deal with long strings

--- chapter18/exercise18_10.cpp
+++ chapter18/exercise18_10.cpp
@@ -11,6 +11,22 @@ int string_length(const char* s) {
     return count;
 }
 
+// compares two strings case-sensitively in lexicographical order
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && *s2) {
+        if (*s1 < *s2) {  // s1 is lexicographically before s2
+            return -1;
+        } else if (*s1 > *s2) {  // s1 is lexicographically after s2
+            return 1;
+        } else {
+            ++s1;
+            ++s2;
+        }
+    }
+
+    return 0;  // s1 equals s2
+}
+
 // 18.7.2 Palindromes using arrays
 bool is_palindrome(const char s[], int n) {  // s points to the first character of an array of n characters
     int first = 0;                           // index of first letter
@@ -23,22 +39,47 @@ bool is_palindrome(const char s[], int n) {  // s points to the first character 
     return true;
 }
 
-istream& read_word(istream& is, char* buffer, int max) {  // read at most max-1 characters from is into buffer
-    is.width(max);                                        // read at most max-1 characters in the next >>
-    is >> buffer;                                         // read whitespace-terminated word, add zero after the last character read into buffer
+// read at most max-1 characters from is into buffer
+istream& read_word(istream& is, char* buffer, int max) {
+    is.width(max);  // read at most max-1 characters in the next >>
+    is >> buffer;   // read whitespace-terminated word, add zero after the last character read into buffer
 
+    if (string_length(buffer) == max - 1) {
+        cout << "read_word() error: Input string is too long!\n";
+        is.clear(ios_base::failbit);  // set the state to fail()
+    }
+    return is;
+}
+
+// read arbitrarily long characters from is into buffer
+istream& read_any_word(istream& is, string& buffer) {
+    getline(is, buffer);
     return is;
 }
 
 int main() {
     try {
-        cout << "Enter a word to check whether it is a palindrome.\n";
-        
-        constexpr int max = 128;
+        cout << "Enter a word to check whether it is a palindrome.\n"
+             << "(Enter 'exit' to break the loop)\n\n";
+
+        cout << "(a) reporting if an input string is too long\n";
+        constexpr int max = 12;
         for (char s[max]; read_word(cin, s, max);) {
+            if (strcmp(s, "exit") == 0) break;
             cout << s << " is";
             if (!is_palindrome(s, string_length(s))) cout << " NOT";
             cout << " a palindrome\n";
+        }
+
+        cout << "\n(b) allowing arbitrarily long string\n";
+        for (string s; read_any_word(cin, s);) {
+            if (s == "exit")
+                break;
+            else if (s != "") {
+                cout << s << " is";
+                if (!is_palindrome(s.c_str(), s.size())) cout << " NOT";
+                cout << " a palindrome\n";
+            }
         }
 
         return 0;
